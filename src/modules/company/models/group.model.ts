a4f240126e054ab0774e57b6f96095d013513c5b
dudosyka <a.hatson22@gmail.com>
2022-12-07 01:19:34 +0300

    GroupModel

---
+++ src/modules/company/models/group.model.ts
@@ -0,0 +1,23 @@
+import { AutoIncrement, BelongsTo, BelongsToMany, Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { CompanyModel } from "./company.model";
+import { UserModel } from "../../user/models/user.model";
+
+@Table
+export class GroupModel extends Model {
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  id: number
+
+  @Column
+  name: string
+
+  @Column
+  company_id: number
+
+  @BelongsTo(() => CompanyModel, "company_id")
+  company: CompanyModel
+
+  @BelongsToMany(() => UserModel, () => UserModel, "group_id", "id")
+  users: UserModel[]
+}
