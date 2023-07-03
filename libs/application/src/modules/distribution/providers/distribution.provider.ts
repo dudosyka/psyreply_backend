a2b9052dcaea965e51ea6130c6a876d3b04c1061
dudosyka <a.hatson22@gmail.com>
2023-07-03 15:32:04 +0300

    Transaction host fix

--- libs/application/src/modules/distribution/providers/distribution.provider.ts
+++ libs/application/src/modules/distribution/providers/distribution.provider.ts
@@ -169,7 +169,7 @@ export class DistributionProvider extends BaseProvider<DistributionModel> {
       throw err;
     });
 
-    this.createBlocks(model, distributionDto.blocks).catch((err) => {
+    await this.createBlocks(model, distributionDto.blocks).catch((err) => {
       TransactionUtil.rollback();
       throw err;
     });
